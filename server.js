ae7f86a97aa9057ce9b9a531d888a3ddb900c13f
Ozioma45 <oziomaegole@gmail.com>
2024-12-23 04:10:44 +0100

    added the routes for the system

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const categoryRoutes = require("./routes/categories");
+const itemRoutes = require("./routes/items");
+
 const { Category, Item } = require("./models");
 
 // Middleware to handle form data
@@ -11,12 +14,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Setting EJS as the view engine
 app.set("view engine", "ejs");
 
-// A test route to confirm our server is running
-app.get("/", (req, res) => {
-  res.send("Welcome to Musical Inventory!");
-});
+app.use("/categories", categoryRoutes);
+app.use("/items", itemRoutes);
 
-app.get("/test", async (req, res) => {
+app.get("/", async (req, res) => {
   try {
     const categories = await Category.findAll({ include: "items" });
     res.json(categories);
